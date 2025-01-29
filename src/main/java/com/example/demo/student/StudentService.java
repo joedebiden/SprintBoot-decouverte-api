bdaa3545c11953b333e731c418eb737bbb6764ba
evan <jeremilafarge@protonmail.com>
2025-01-29 01:01:07 +0100

    🎨: JPA repository added

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,15 +9,15 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1,
-                        45,
-                        LocalDate.of(1980, 12, 5),
-                        "jean",
-                        "jean.dupont@gmail.com"
-                )
-        );
+        return studentRepository.findAll();
     }
 }
