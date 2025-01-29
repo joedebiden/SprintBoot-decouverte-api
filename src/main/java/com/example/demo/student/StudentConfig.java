41e05fa0ec9d423a4a92a6d08e70803d67aa925e
evan <jeremilafarge@protonmail.com>
2025-01-29 01:17:08 +0100

    🧐: Data saving for student

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.cglib.core.Local;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+
+            Student jean = new Student(
+                    "Jean",
+                    "jean.mail@gmail.com",
+                    LocalDate.of(1980, Month.JANUARY, 8),
+                    45
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex.mail@gmail.com",
+                    LocalDate.of(1990, Month.MARCH, 28),
+                    35
+            );
+
+            studentRepository.saveAll(
+                    List.of(jean, alex)
+            );
+        };
+    }
+}
